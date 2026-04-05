36263d63a3fea5c55b41ba3c3c59135575251b58
rickypatel5112 <rickypatel5112@gmail.com>
2026-04-05 15:32:50 -0400

    Update config to use HTTP if certs not found

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
 import fs from 'fs';
+import path from 'path';
 
 import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 
-// https://vite.dev/config/
+// Paths to your certs
+const keyPath = path.resolve(__dirname, 'localhost-key.pem');
+const certPath = path.resolve(__dirname, 'localhost.pem');
+
+const httpsOptions =
+  fs.existsSync(keyPath) && fs.existsSync(certPath)
+    ? {
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath),
+      }
+    : undefined;
+
+console.log(
+  httpsOptions
+    ? 'Starting Vite in HTTPS mode 🔒'
+    : 'Starting Vite in HTTP mode 🌐',
+);
+
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   server: {
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem'),
-    },
+    https: httpsOptions,
   },
 });
